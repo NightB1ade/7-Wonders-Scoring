0a96ab6c06d6d2d1071badff576f84cbb829b38f
flodskum <flodskum@users.noreply.github.com>
2020-03-10 18:47:24 +0000

    Version 4 improvements
    * Added total track
    * Dynamically updates total as you enter scores
    * Its a bit cumbersome as it updates all scores every time
    * Changed fragment_line to not contain player 1 score to allow for setting id and tag

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # 7-Wonders-Scoring
 
-## Version 3
+## Version 4
 
 * Simple app prototype
 * First screen is player name entry - list of players names built and shown below
-* Second screen is dynamic to number of players and sets up score tracks based on config file
+* Second screen is dynamic to number of players, and sets up score tracks based on config file
+* Totals are now tracked dynamically
 
 ## Next steps
 Current brain dump - also see TODOs in code
@@ -14,7 +15,8 @@ Current brain dump - also see TODOs in code
 - [x] default player names to "player X" and pre-selected for easy edit/entry
 - [x] create game config xml file and parser
 - [x] make score tracks dynamic based on read in data config
-- [ ] add total track
+- [x] add total track
+- [ ] make "Enter" on score entry go across and not down
 - [ ] allow removal of players on first screen?
 - [ ] player name editing
 - [ ] menu to reset and return to main screen
--- app/src/main/java/com/stivestabletop/scorer/ScoreActivity.kt
+++ app/src/main/java/com/stivestabletop/scorer/ScoreActivity.kt
@@ -3,7 +3,9 @@ package com.stivestabletop.scorer
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
 import android.text.InputType
+import android.text.TextWatcher
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +15,8 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -25,12 +29,13 @@ fun getContrastTextColour(bg: Int): Int {
 
 class TrackFragment : Fragment() {
     companion object {
-        fun newInstance(players: Int, name: String, colour: String): TrackFragment {
+        fun newInstance(players: Int, name: String, colour: String, total: Boolean): TrackFragment {
             val f = TrackFragment()
             val args = Bundle()
             args.putInt("players", players)
             args.putString("name", name)
             args.putString("colour", colour)
+            args.putBoolean("total", total)
             f.setArguments(args)
             return f
         }
@@ -46,14 +51,17 @@ class TrackFragment : Fragment() {
         var players = 0
         var tname = ""
         var tcol = ""
+        var total = false
         if (args != null) {
             players = args.getInt("players", 0)
             tname = args.getString("name", "track")
             tcol = "#" + args.getString("colour", "FFFFFF")
+            total = args.getBoolean("total", false)
         }
         // Create new track fragment
         val track = inflater.inflate(R.layout.fragment_line, container, false)
-
+        if (total)
+            track.id = R.id.total_track
         // Update the track name/colour
         val name = track.findViewById<TextView>(R.id.trackName)
         name.setText(tname)
@@ -63,29 +71,92 @@ class TrackFragment : Fragment() {
 
         // Sort out score boxes
         val view = track.findViewById<LinearLayout>(R.id.scoreLayout)
-        // We already have one player's score box, add the others
-        while (players > 1) {
-            view?.addView(getScore())
-            players--
-        }
+        for (num in 1..players)
+            view?.addView(getScoreView(num, tname, !total))
         return track
     }
 
     // Player name view box
     @RequiresApi(Build.VERSION_CODES.KITKAT)
-    fun getScore(): EditText {
+    fun getScoreView(id: Int, tag: String, editable: Boolean): View {
         val layoutparams = LinearLayout.LayoutParams(
             0,
             LinearLayout.LayoutParams.WRAP_CONTENT,
             1.0f
         )
-        val tv_dynamic = EditText(requireContext())
-        tv_dynamic.textSize = 12f
-        tv_dynamic.inputType = InputType.TYPE_CLASS_NUMBER or
-                InputType.TYPE_NUMBER_VARIATION_NORMAL or InputType.TYPE_NUMBER_FLAG_SIGNED
-        tv_dynamic.layoutParams = layoutparams
-        //tv_dynamic.gravity = Gravity.CENTER
-        return tv_dynamic
+        val view = if (editable) EditText(requireContext()) else TextView(requireContext())
+        view.id = id
+        view.tag = tag
+        view.textSize = 12f
+        view.layoutParams = layoutparams
+        if (editable) {
+            view.inputType = InputType.TYPE_CLASS_NUMBER or
+                    InputType.TYPE_NUMBER_VARIATION_NORMAL or InputType.TYPE_NUMBER_FLAG_SIGNED
+            view.gravity = Gravity.END
+
+            view.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable) {}
+                override fun beforeTextChanged(
+                    s: CharSequence, start: Int,
+                    count: Int, after: Int
+                ) {
+                }
+
+                override fun onTextChanged(
+                    s: CharSequence, start: Int,
+                    before: Int, count: Int
+                ) {
+                    // TODO: Do we have to re-calculate everything?
+                    // TODO: Does this have to be soo ugly...
+                    // TODO: Could be a function call that is called from here rather than duplicated!
+                    // Collect all the scores for all the players in all the tracks
+                    val scores = mutableMapOf<Int, Int>()
+                    val fragments = activity?.supportFragmentManager?.fragments
+                    if (fragments != null) {
+                        for (fragment in fragments) {
+                            if (fragment.id != R.id.total_track) {
+                                val view = fragment.view
+                                val layout = view?.findViewById<LinearLayout>(R.id.scoreLayout)
+                                val children = layout?.children
+                                if (children != null)
+                                    for (child in children) {
+                                        if (child is EditText) {
+                                            val text = child.text.toString()
+                                            var value: Int
+                                            try {
+                                                value = text.toInt()
+                                            } catch (e: NumberFormatException) {
+                                                // Blank or maybe a negative symbol
+                                                value = 0
+                                            }
+                                            if (scores.containsKey(child.id)) {
+                                                scores[child.id] = scores.getValue(child.id) + value
+                                            } else {
+                                                scores[child.id] = value
+                                            }
+                                        }
+                                    }
+                            }
+                        }
+                    }
+                    // Update the totals in the total track
+                    val track = activity?.findViewById<ConstraintLayout>(R.id.total_track)
+                    val layout = track?.findViewById<LinearLayout>(R.id.scoreLayout)
+                    val children = layout?.children
+                    if (children != null)
+                        for (child in children) {
+                            if (child is TextView) {
+                                child.text = scores[child.id].toString()
+                            }
+                        }
+                }
+            })
+        } else {
+            view.text = "0"
+            view.height = 100
+            view.gravity = Gravity.CENTER_HORIZONTAL or Gravity.END
+        }
+        return view
     }
 }
 
@@ -121,9 +192,13 @@ class ScoreActivity : AppCompatActivity() {
                     if (track.default)
                         trans.add(
                             R.id.trackLayout,
-                            TrackFragment.newInstance(players, track.name, track.colour)
+                            TrackFragment.newInstance(players, track.name, track.colour, false)
                         )
                 }
+                trans.add(
+                    R.id.trackLayout,
+                    TrackFragment.newInstance(players, "Total", "000000", true)
+                )
                 trans.commit()
             }
         }
@@ -137,11 +212,11 @@ class ScoreActivity : AppCompatActivity() {
             LinearLayout.LayoutParams.WRAP_CONTENT,
             1.0f
         )
-        val tv_dynamic = TextView(this)
-        tv_dynamic.textSize = 14f
-        tv_dynamic.text = string
-        tv_dynamic.layoutParams = layoutparams
-        tv_dynamic.gravity = Gravity.CENTER
-        return tv_dynamic
+        val view = TextView(this)
+        view.textSize = 14f
+        view.text = string
+        view.layoutParams = layoutparams
+        view.gravity = Gravity.CENTER
+        return view
     }
 }
